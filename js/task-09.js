e157888348b9f761092b5e1ae75a11b5e5c66107
Hintln <andrejlityprog@gmail.com>
2022-10-25 21:39:07 +0300

    +

--- js/task-09.js
+++ js/task-09.js
@@ -14,10 +14,11 @@ function getRandomHexColor() {
 const bodyRef = document.querySelector('body');
 const spanRef = document.querySelector('.color');
 const buttonRef = document.querySelector('.change-color');
-const color = getRandomHexColor()
 
 
 buttonRef.addEventListener('click', (e) => {
-  spanRef.textContent = `${color}`;
+  spanRef.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   bodyRef.style.backgroundColor = spanRef.textContent;
 });
+
+
